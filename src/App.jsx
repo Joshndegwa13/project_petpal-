d6b741515bf98de0804943ccc2c9afc8b3b38e23
terenceouma <doros.devs@gmail.com>
2024-09-18 13:00:22 +0300

    moved the project from the previous repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Viewuserprofile from "../src/components/Viewuserprofile";
+import Home from "../src/components/Home";
+import Settings from "../src/components/Settings";
+import AvatarUpload from "../src/components/AvatarUpload";
+import { UserProvider } from "../src/context/UserContext";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <UserProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Viewuserprofile />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/avatar-upload" element={<AvatarUpload />} />{" "}
+          </Routes>
+        </div>
+      </Router>
+    </UserProvider>
   );
 }
 
